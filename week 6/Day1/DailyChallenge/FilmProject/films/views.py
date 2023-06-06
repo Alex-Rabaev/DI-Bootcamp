d210cb92fc332409cc442b897d982e2f239bd18e
Alex-Rabaev <rabaevav@live.com>
2023-06-07 00:57:13 +0300

    Week 6, Day 1, DailyChallenge

---
+++ week 6/Day1/DailyChallenge/FilmProject/films/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Film, Country, Category, Director, Review
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import FilmForm, DirectorForm, ReviewForm
+
+
+# Create your views here.
+
+
+class HomePageView(ListView):
+    template_name = "homepage.html"
+    context_object_name = "films"
+
+    def get_queryset(self):
+        return Film.objects.all()
+
+
+class FilmCreateView(CreateView):
+    model = Film
+    form_class = FilmForm
+    template_name = "film/add_film.html"
+    success_url = reverse_lazy("homepage")
+
+
+class DirectorCreateView(CreateView):
+    model = Director
+    form_class = DirectorForm
+    template_name = "director/add_director.html"
+    success_url = reverse_lazy("homepage")
+
+
+class ReviewCreateView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "review/add_review.html"
+    success_url = reverse_lazy("homepage")
